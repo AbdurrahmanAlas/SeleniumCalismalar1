1ee9ce6c94d5024bf896363d5d930ddb2aea1866
Abdurrahman Alas <63901054+AbdurrahmanAlas@users.noreply.github.com>
2023-01-01 16:25:13 +0300

    2.degisiklik

--- src/test/java/SeleniumProje1.java
+++ src/test/java/SeleniumProje1.java
@@ -1,6 +1,8 @@
 public class SeleniumProje1 {
     public static void main(String[] args) {
         System.out.println("abdurrahman projesinde calısıyorum");
+        System.out.println("abdurrahman projesinde22 calısıyorum");
+        System.out.println("abdurrahman projesinde23 calısıyorum");
     }
 
 }
