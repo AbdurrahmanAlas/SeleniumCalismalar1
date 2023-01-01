a77510914b2d055739f5438603b1fd517951393f
Abdurrahman Alas <63901054+AbdurrahmanAlas@users.noreply.github.com>
2023-01-01 16:27:40 +0300

    main branch degissiklik

--- src/test/java/SeleniumProje1.java
+++ src/test/java/SeleniumProje1.java
@@ -1,6 +1,8 @@
 public class SeleniumProje1 {
     public static void main(String[] args) {
         System.out.println("abdurrahman projesinde calısıyorum");
+        System.out.println("main branch degisiklik mi kullanacaksın");
+        System.out.println("main branch degisiklik mi kullanacaksın");
     }
 
 }
